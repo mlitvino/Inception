116a09583c52e6b5930ce438556341ed23e00a99
mlitvino <mlitvino@vbox.myguest.virtualbox.org>
2025-10-26 23:22:04 +0200

    add new build version

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -2,17 +2,22 @@
 set -e
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Initializing database..."
-    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+  mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 fi
 
 MYSQL_ROOT_PASS="$(cat /run/secrets/mysql_root_pass)"
 MYSQL_USER_PASS="$(cat /run/secrets/mysql_user_pass)"
-cat > /etc/mysql/init.sql << EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';
+
+# escape single quotes in passwords for SQL
+esc() { printf "%s" "$1" | sed "s/'/''/g"; }
+ROOT_ESC="$(esc "$MYSQL_ROOT_PASS")"
+USER_ESC="$(esc "$MYSQL_USER_PASS")"
+
+cat > /etc/mysql/init.sql <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_ESC}';
 CREATE DATABASE IF NOT EXISTS \`wordpress\` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER IF NOT EXISTS 'mlitvino'@'%' IDENTIFIED BY '${MYSQL_USER_PASS}';
-GRANT ALL PRIVILEGES ON \`mariadb\`.* TO 'mlitvino'@'%';
+CREATE USER IF NOT EXISTS 'mlitvino'@'%' IDENTIFIED BY '${USER_ESC}';
+GRANT ALL PRIVILEGES ON \`wordpress\`.* TO 'mlitvino'@'%';
 FLUSH PRIVILEGES;
 EOF
 
--- srcs/requirements/wordpress/init.sh
+++ srcs/requirements/wordpress/init.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
+set -e
 cd /var/www/html
 
 WP_ADMIN_PASS="$(cat /run/secrets/admin_pass)"
 WP_USER_PASS="$(cat /run/secrets/user_pass)"
+WP_DB_PASS="$(cat /run/secrets/mysql_user_pass)"
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+DB_NAME="${WP_DB_NAME:-wordpress}"
+DB_USER="${WP_DB_USER:-mlitvino}"
+DB_HOST="${DB_HOST:-mariadb}"
+
+curl -sS -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress \
-	--dbuser=wpuser \
-	--dbpass=password \
-	--dbhost=mariadb \
-	--allow-root
 
-./wp-cli.phar core install --url="${WP_URL}" \
-	--title="${WP_TITLE}" \
-	--admin_user="${WP_ADMIN_NAME}" \
-	--admin_password="${WP_ADMIN_PASS}" \
-	--admin_email="${WP_ADMIN_EMAIL}" \
-	--allow-root
+if [ ! -f wp-config.php ]; then
+  ./wp-cli.phar config create \
+    --dbname="$DB_NAME" \
+    --dbuser="$DB_USER" \
+    --dbpass="$WP_DB_PASS" \
+    --dbhost="$DB_HOST" \
+    --allow-root
+fi
 
-./wp-cli.phar user create \
-	"${WP_USER_NAME}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PASS}" \
-	--role=author --allow-root
+if ! ./wp-cli.phar core is-installed --allow-root >/dev/null 2>&1; then
+  ./wp-cli.phar core install \
+    --url="${WP_URL}" \
+    --title="${WP_TITLE}" \
+    --admin_user="${WP_ADMIN_NAME}" \
+    --admin_password="${WP_ADMIN_PASS}" \
+    --admin_email="${WP_ADMIN_EMAIL}" \
+    --skip-email \
+    --allow-root
+fi
 
-php-fpm8.2 -F
+if [ -n "${WP_USER_NAME:-}" ] && ! ./wp-cli.phar user get "$WP_USER_NAME" --allow-root >/dev/null 2>&1; then
+  ./wp-cli.phar user create "$WP_USER_NAME" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASS" --role=author --allow-root
+fi
+
+exec php-fpm8.2 -F
